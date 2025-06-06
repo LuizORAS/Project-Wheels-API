2a9588a6695209ece3251b3255b2626acbcb14df
Luiz Lima <luizfellipe744@gmail.com>
2025-06-06 00:17:28 -0300

    bug fixing for bike seeding

--- src/main/java/org/wheels/model/Bike.java
+++ src/main/java/org/wheels/model/Bike.java
@@ -4,11 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Bike {
     private int id;
     private BikeType type;
     private boolean available;
+
+    public Bike() {}
+    public Bike(int id, BikeType type, boolean available) {
+        this.id = id;
+        this.type = type;
+        this.available = available;
+    }
+
+    public int getId() { return id; }
+    public BikeType getType() { return type; }
+
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
 }
